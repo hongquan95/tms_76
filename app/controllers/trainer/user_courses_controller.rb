6f94c56e719c4f96e542e3b6f490824530bd3feb
hongquan95 <hongquan95.tran@gmail.com>
2018-03-12 09:08:47 +0700

    rebase commit

--- app/controllers/trainer/user_courses_controller.rb
+++ app/controllers/trainer/user_courses_controller.rb
@@ -2,8 +2,19 @@ class Trainer::UserCoursesController < ApplicationController
   before_action :logged_in_user, :verify
 
   def index
-    @user_courses = current_user.courses.paginate page: params[:page], per_page: Settings.page_course
+    @user_courses = current_user.courses.paginate page: params[:page]
   end
 
-  def show; end
+  def show
+    # byebug
+    @user_course = UserCourse.find(params[:id])
+    @users = User.all
+  end
+
+  private
+
+    def user_course_params
+      params.require(:user_courses).permit(user_courses_attributes: [:id, :user_id ])
+
+    end
 end
--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,4 +1,6 @@
 class UserCourse < ApplicationRecord
   belongs_to :user
   belongs_to :course
+  scope :find_user, ->(id){where "course_id = ?",id}
+  scope :find_user_not_in_course, ->(id){where "course_id NOT IN (?)",id}
 end
