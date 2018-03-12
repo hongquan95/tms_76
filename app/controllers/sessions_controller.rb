20ea6430da8a544321567cf2eeaf3574f37120c9
hongquan95 <hongquan95.tran@gmail.com>
2018-03-12 17:39:12 +0700

    finish user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  before_action :load_user, only: :create
+  def new; end
+
+  def create
+    # byebug
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      byebug
+      redirect_to @user
+    else
+      flash.now[:danger] = t "sessions.create.invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+  def load_user
+    @user = User.find_by email: params[:session][:email].downcase
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'users/new'
+
+  get 'users/show'
+
   root "static_pages#home"
   get "/help", to: "static_pages#help"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :users
 end
