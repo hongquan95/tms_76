7e2a0153b5ed7052fba4e8992f2be67d80b924d9
hongquan95 <hongquan95.tran@gmail.com>
2018-03-12 09:08:47 +0700

    Design layout user (form-login, index user, edit user)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,61 @@
+class UsersController < ApplicationController
+  before_action :load_user, only: %i(show)
+  before_action :logged_in_user, only: %i(index update edit)
+  before_action :correct_user, only: %i(update edit)
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash[:success] = t "users.create.welcome"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    return render :edit unless @user.update_attributes user_params
+    flash[:success] = t "users.update.notice"
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation, :role
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "users.edit.require"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    load_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    @user || render(file: "public/404.html", status: 404, layout: true)
+  end
+end
