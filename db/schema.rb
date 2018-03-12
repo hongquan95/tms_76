2f8d69c09b3941aada8b1a2127a9322effcc0f53
hongquan95 <hongquan95.tran@gmail.com>
2018-03-12 09:08:47 +0700

    Admin show alluser, edit user, add and delete user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312023519) do
+ActiveRecord::Schema.define(version: 20180313102915) do
 
   create_table "course_subject_tasks", force: :cascade do |t|
     t.integer "course_id"
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 20180312023519) do
     t.string "password_digest"
     t.integer "role"
     t.string "school"
-    t.date "graduation"
+    t.string "graduation"
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
